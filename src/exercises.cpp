bd49b9173884c77ab2dfdfadbeff22c06dee1e1e
JaimeMartinezH <159574667+JaimeMartinezH@users.noreply.github.com>
2024-04-14 07:29:37 +0200

    Ejercicio 1 prueba 1

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -5,8 +5,41 @@
 using namespace std;
 
 void exercise_1(string s1) {
-  cout<<"Hola";
-cout<<"Soy jaime inge";
+<<<<<<< HEAD
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::vector<int> encontrar_indices(const std::string & texto) {
+    std::vector<int> indices;
+    int indice = 0;
+
+    for (char c : texto) {
+        if (c == ' ') {
+
+            indices.push_back(indice);
+        }
+        indice++;
+    }
+
+    indices.push_back(indice);
+
+    return indices;
+}
+
+int main() {
+    std::string texto;
+    std::cout << "Ingrese una línea de texto:" << std::endl;
+    std::getline(std::cin, texto);
+
+    std::vector<int> indices = encontrar_indices(texto);
+
+    for (int indice : indices) {
+        std::cout << indice << std::endl;
+    }
+
+    return 0;
+}
 }
 
 void exercise_2(string s1) {
